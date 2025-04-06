226f95752423c098ba833521f8851cf396a9b6c0
Thomas Kolderup Selvig <thomas.selvig.02@gmail.com>
2025-04-06 03:57:20 +0200

    remove old

--- public/env.lua
+++ public/env.lua
@@ -5,10 +5,8 @@ env.chestSide = "below"
 env.blacklistOres = {}
 env.trashItems = {}
 
--- Configuration
--- env.WEBSOCKET_URL = "wss://sought-composed-alpaca.ngrok-free.app/turtles"
 env.WEBSOCKET_URL = "wss://sought-composed-alpaca.ngrok-free.app"
--- env.WEBSOCKET_URL = "ws://81.0.169.192:1337/turtles"
+-- env.WEBSOCKET_URL = "ws://81.0.169.192:1337"
 env.TURTLE_NAME = os.getComputerLabel() or tostring(os.getComputerID())
 env.HEARTBEAT_INTERVAL = 3 -- seconds
 env.CONNECTION_TIMEOUT = 10 -- seconds to wait before considering connection lost
